6cc9b0a02ed59529571beeeccd3ced1c67b0b5bc
Charles Matthews <charles0matthews@gmail.com>
2024-08-12 16:03:25 -0400

    tidy code to archive

--- 6xswitches/6xswitches.ino
+++ 6xswitches/6xswitches.ino
@@ -1,10 +1,9 @@
-//Six switches to MIDI: removing connection triggers sound
-//To connect to Max patch
+// Six switches to MIDI: breaking the connection triggers a sound
+// To connect to Max/MSP
 
 int switches[] = {0, 0, 0, 0, 0, 0};
 #include <MIDIUSB.h>
 MIDIEvent e;
-
 
 void setup() {
   Serial.begin(9600);
@@ -16,8 +15,10 @@ void setup() {
 void loop() {
   for (int i=0; i < 6; i++) {
     int value = digitalRead(i + 2);
-//    uncomment the line below to invert the values
-//    int value = 1 - digitalRead(i + 2);
+    
+    // uncomment the line below to invert the values
+    // int value = 1 - digitalRead(i + 2);
+    
     if (value != switches[i]){
       Serial.print("switch");
       Serial.print( i);
@@ -25,19 +26,15 @@ void loop() {
       Serial.println(value);
       switches[i] = value;
 
-        e.m1 = 176; //label as cc, channel 1
-        e.m2 = i; //cc lane from sensor index number   
-        e.m3 = switches[i]; //set the value from the sensor
-        e.type = 8; 
+      e.m1 = 176; //label as cc, channel 1
+      e.m2 = i; //cc lane from sensor index number   
+      e.m3 = switches[i]; //set the value from the sensor
+      e.type = 11; 
                       
-        MIDIUSB.write(e);
-
+      MIDIUSB.write(e);
       
     }
     
-    
   }
-  
-  
-  
+
 }
